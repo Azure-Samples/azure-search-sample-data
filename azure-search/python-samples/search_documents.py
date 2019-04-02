aaf44d346d348aa018030fe34d088c06bcf5e8ae
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2019-04-02 13:33:45 -0700

    Create search_documents.py

---
+++ azure-search/python-samples/search_documents.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import http.client, urllib.parse, json
+
+# **********************************************
+# *** Update or verify the following values. ***
+# **********************************************
+
+# Replace this with a valid service name.
+service_name = 'ENTER SERVICE NAME HERE'
+
+# Replace this with a valid subscription key.
+subscriptionKey = 'ENTER KEY HERE'
+
+index_name = 'test-index'
+
+host = service_name + '.search.windows.net'
+operation = '/indexes/' + index_name + '/docs/search'
+api_version = '2017-11-11'
+params = 'api-version=' + api_version
+path = operation + '?' + params
+
+request = {
+    "search": "*"
+}
+
+def pretty_print (content):
+# Note: We convert content to and from an object so we can pretty-print it.
+	return json.dumps(json.loads(content), indent=4)
+
+def post (host, path, content):
+	print ('Calling POST ' + host + path + '.')
+	headers = {
+		'api-key': subscriptionKey,
+		'Content-Type': 'application/json',
+		'Content-Length': len (content)
+	}
+	conn = http.client.HTTPSConnection(host)
+	conn.request ("POST", path, content, headers)
+	response = conn.getresponse ()
+	return response.status, response.read ()
+
+# Convert the request to a string.
+content = json.dumps(request)
+status, result = post (host, path, content)
+print ('Status code: ' + str(status))
+print (pretty_print(result))
