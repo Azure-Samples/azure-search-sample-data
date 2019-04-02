2932576774d268816d29cf3bc4724cdd1114458d
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2019-04-02 13:24:01 -0700

    Create delete_index.js

---
+++ azure-search/nodejs-samples/delete_index.js
@@ -0,0 +1,83 @@
+'use strict';
+
+let https = require('https');
+
+// **********************************************
+// *** Update or verify the following values. ***
+// **********************************************
+
+// Replace this with a valid service name.
+let service_name = 'ENTER SERVICE NAME HERE';
+
+// Replace this with a valid subscription key.
+let subscriptionKey = 'ENTER KEY HERE';
+
+let index_name = 'test-index';
+
+let host = service_name + '.search.windows.net';
+let operation = '/indexes/' + index_name;
+let api_version = '2017-11-11';
+let params = {
+    'api-version' : api_version
+}
+
+/*
+ * Helper functions.
+ */
+
+let get_path = function (operation, params) {
+    var pairs = [];
+    for (var key in params) {
+        pairs.push(key + '=' + params[key]);
+    }
+    return operation + '?' + pairs.join('&');
+};
+
+// callback is the function to call once we have the entire response.
+let response_handler = function (callback, response) {
+    let body = '';
+    response.on('data', function (d) {
+        body += d;
+    });
+    response.on('end', function () {
+        // Call the callback function with the response and body.
+        callback({ response: response, body: body });
+    });
+    response.on('error', function (e) {
+        console.log('Error: ' + e.message);
+    });
+};
+
+// Get an HTTP response handler that calls the specified callback function once we have the entire response.
+let get_response_handler = function (callback) {
+    // Return a function that takes an HTTP response, and is closed over the specified callback.
+    // This function signature is required by https.request, hence the need for the closure.
+    return function (response) {
+        response_handler(callback, response);
+    }
+};
+
+// callback is the function to call once we have the entire response from the GET request.
+let send_delete = function (host, path, callback) {
+    let request_params = {
+        method: 'DELETE',
+        hostname: host,
+        path: path,
+        headers: {
+            'api-key': subscriptionKey,
+        }
+    };
+
+    console.log('Calling DELETE ' + host + path + '.');
+    // Pass the callback function to the response handler.
+    let req = https.request(request_params, get_response_handler(callback));
+    req.end();
+};
+
+/*
+ * Send the request and provide a callback function that prints the response.
+ */
+
+send_delete(host, get_path(operation, params), function (result) {
+    console.log ('Status code: ' + result.response.statusCode)
+});
